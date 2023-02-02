dd3aac2ecf0f367514261631a6aeb71e14de5421
Irene Lopez <elcorreodeirene@gmail.com>
2023-02-02 15:42:50 +0100

    Updates

---
+++ precache-manifest.7d0aa4212a0839d5d3880c52ea983f8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6b013b86bfd2d6eeae9",
+    "url": "/Knit-Tools/static/css/main.e02bf223.chunk.css"
+  },
+  {
+    "revision": "e6b013b86bfd2d6eeae9",
+    "url": "/Knit-Tools/static/js/main.e6b013b8.chunk.js"
+  },
+  {
+    "revision": "0258e1ac1b28aa137e2a",
+    "url": "/Knit-Tools/static/js/1.0258e1ac.chunk.js"
+  },
+  {
+    "revision": "fddb1e81b3841ebf8159",
+    "url": "/Knit-Tools/static/js/runtime~main.fddb1e81.js"
+  },
+  {
+    "revision": "8a21957e45fdd5957cbdaef80bdd6af1",
+    "url": "/Knit-Tools/index.html"
+  }
+];
